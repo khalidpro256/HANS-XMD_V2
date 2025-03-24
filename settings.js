c93b4bafc33feee627f7cf646d51575fe2056b38
khalidpro256 <khalidpro536@gmail.com>
2025-03-24 18:56:05 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,11 +9,11 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "255756530143"
-global.ownername = "HANSTZ"
+global.ownernomer = "256770178719"
+global.ownername = "SEGZILLOR"
 global.ytname = "HANSTZ-TECH"
 global.socialm = "GitHub: Mrhanstz"
-global.location = "AFRICA, TANZANIA, DODOMA"
+global.location = "AFRICA, UGANDA, KAMPALA"
 
 global.ownernumber = '255756530143'  //creator number
 global.ownername = 'HANSTZ' //owner name
@@ -38,11 +38,11 @@ global.autoRecording = true //auto recording
 global.autoTyping = true //auto typing
 global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = true //auto update bio
+global.autobio = false  //auto update bio
 global.anti91 = false //auto block +91 
 global.autoswview = true //auto view status/story
-global.autoreact = true // This is For Auto React 
-global.welcome = true // For Welcome massage groups
+global.autoreact = false // This is For Auto React 
+global.welcome = false // For Welcome massage groups
 global.autoreply = true //Is For Autoreply massage When Your Offline the bot must be reply massages
 
 //menu type 
